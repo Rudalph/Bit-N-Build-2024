bcc12a41205fd96cd07f814ad0b1b84a4f529f9b
Rudalph <gonsalvesrudalph@gmail.com>
2024-02-17 08:57:58 +0530

    UI fixes

--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -39,7 +39,7 @@ const page = () => {
  
   return (
     <section className='mt-1 mr-1'>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-1">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign up</h2>
@@ -110,7 +110,7 @@ const page = () => {
                   </div>
                 <div className='flex justify-between'>
                 <input
-                      className="flex h-10 w-auto  rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      className="flex h-10 w-50 rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                       type="password"
                       placeholder="OTP"
                       id="password"
@@ -118,10 +118,10 @@ const page = () => {
                     ></input>
                  <button
                     type="button"
-                    className="inline-flex h-10 w-auto items-center justify-center rounded-md border border-black px-3.5 py-2.5 font-semibold leading-7 text-black hover:bg-slate-600"
+                    className="inline-flex h-10 w-40 items-center justify-center rounded-md border border-black px-3.5 py-2.5 font-semibold leading-7 text-black hover:bg-slate-600"
                     onClick={generate_send_otp}
                   >
-                    Create Account <ArrowRight className="ml-2" size={16} />
+                    Generate OTP <ArrowRight size={16} />
                   </button>
                 </div>
                 <div>
@@ -157,13 +157,13 @@ const page = () => {
             </div>
           </div>
         </div>
-        <div className="h-full w-full">
+        {/* <div className="h-full w-full">
           <img
             className="mx-auto h-full w-full rounded-md object-cover"
             src="https://images.unsplash.com/photo-1559526324-4b87b5e36e44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
             alt=""
           />
-        </div>
+        </div> */}
       </div>
     </section>
   )
