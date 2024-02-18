2f37780c28631df677dba533c24a6cd16080f88a
JonathanDabre <jonathandabre01@gmail.com>
2024-02-18 13:24:36 +0530

    added nextjs website

---
+++ resume-parser/app/api/textextract/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest,NextResponse } from 'next/server'
+import { TextLoader } from 'langchain/document_loaders/fs/text'
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { join } from 'path'
+import fs, { writeFile,unlink } from 'fs/promises';
+import pdfParse from 'pdf-parse';
+
+export async function POST(request: NextRequest) {
+    console.log("1")
+    const data = await request.formData()
+    const file: File | null = data.get('file') as unknown as File
+    console.log(file)
+  
+    if(!file) {
+      return NextResponse.json({ success: false })
+    }
+    
+    console.log("2")
+    const bytes = await file.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+  
+    const path = join('documents',file.name)
+    await writeFile(path, buffer)
+
+    try{
+        const text = await fs.readFile(path);
+        console.log("3")
+        const parsedData = await pdfParse(text);
+        console.log(parsedData)
+    }
+    catch (err) {
+        console.error('Error reading or parsing PDF:', err);
+    }
+
+    console.log(`open ${path} to see the uploaded file`)
+  
+    const loader = new DirectoryLoader('./documents', {
+      ".txt": (path) => new TextLoader(path),
+      ".md": (path) => new TextLoader(path),
+      ".pdf": (path) => new PDFLoader(path)
+    })
+  
+    const docs = await loader.load()
+    console.log(`Docs go here: `)
+    console.log(docs)
+    // Use the unlink function to delete the file asynchronously.
+    await unlink(path);
+  
+    return NextResponse.json({
+      "data": 'successfully created index and loaded data into pinecone...'
+    })
+  }
---
+++ resume-parser/app/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+
+export default function Home() {
+
+  const [file, setFile] = useState<File >();
+
+
+  const handleUploadClick = async(e: React.FormEvent<HTMLFormElement>) => {
+        
+    e.preventDefault()
+
+    if(!file) return  //Return if theres no file
+    
+    try {
+      const data = new FormData()
+      data.set('file',file)
+      console.log(typeof(data))
+      console.log(data)
+      const result = await fetch('/api/textextract',{
+        method: "POST",
+        body: data
+      })
+      const json = await result.json()
+      console.log('result: ', json)
+    } catch (err) {
+      console.log('err:', err)
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // Call both functions here
+    await handleUploadClick(e);
+  };
+
+  return (
+    <>
+      <div className="flex justify-center my-10">
+
+        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row" >
+          <input type="file" name="file"   onChange={(e) => setFile(e.target.files?.[0])} className={`file:cursor-pointer my-2  text-xs sm:text-sm file:text-sm file:py-1 sm:file:py-3 file:px-4 file:rounded-full file:text-white file:shadow-none   file:bg-[#007C7C] file:hover:bg-[#007c87] file:border-none`}/>
+                  
+          <input type="submit" value='Submit' className={`doc-upload-button cursor-pointer bg-[#EDFFFF] border border-[#007C7C] text-[#007C7C] font-semibold my-2 py-1 sm:py-3 px-8 text-sm rounded-full`}/>
+                  
+        </form>
+      </div>
+      <div className="">
+
+      </div>
+
+    </>
+  );
+}
